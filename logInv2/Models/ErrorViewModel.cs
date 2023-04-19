96b14f996c718aba7969a001f40ca910f2a023bb
SAZZAD HOSSAIN TASNIM <sazzadaiub1@gmail.com>
2023-04-19 14:06:47 +0600

    Add project files.

---
+++ logInv2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace logInv2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
